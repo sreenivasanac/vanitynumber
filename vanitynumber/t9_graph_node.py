23a7b41edbce6cba7df16b6b48ef4ab5635de7bc
Sreenivasan AC <sreenivasan.nitt@gmail.com>
2019-07-19 23:22:09 -0400

    Updated logic to sort the results of showing the top wordified results, now based on longest subword first, then max_continous_letters, then number of total letters

--- vanitynumber/t9_graph_node.py
+++ vanitynumber/t9_graph_node.py
@@ -1,23 +1,32 @@
 # Graph Node for T9 Dictionary Wordifier
 class T9_Graph_Node(object):
-    def __init__(self, wordified_so_far, index_so_far, number_of_chars_in_word, max_number_of_continous_chars_in_word):
+    def __init__(self, wordified_so_far, index_so_far, number_of_chars_in_word, max_length_word_substring, max_number_of_continous_chars_in_word):
         self.wordified_so_far = wordified_so_far
         self.index_so_far = index_so_far
         self.number_of_chars_in_word = number_of_chars_in_word
         self.max_number_of_continous_chars_in_word = max_number_of_continous_chars_in_word
+        self.max_length_word_substring = max_length_word_substring
 
     # Comparator functions, for sorting, and min, max
-    # If both nodes contain same number of characters in the word, then compare max number of continous characters between them
+    # If both nodes contain same number of characters in the word, then compare length of word substring present
+    # If that is also equal, then compare number of continous characters between them
     def __le__(self, other):
-        return (self.max_number_of_continous_chars_in_word < other.max_number_of_continous_chars_in_word) or \
-            ((self.max_number_of_continous_chars_in_word == other.max_number_of_continous_chars_in_word) and \
+        return (self.max_length_word_substring < other.max_length_word_substring) or \
+            ((self.max_length_word_substring == other.max_length_word_substring) and \
+                (self.max_number_of_continous_chars_in_word < other.max_number_of_continous_chars_in_word)) or \
+            ((self.max_length_word_substring == other.max_length_word_substring) and \
+                (self.max_number_of_continous_chars_in_word == other.max_number_of_continous_chars_in_word) and \
                 (self.number_of_chars_in_word < other.number_of_chars_in_word))
 
     def __eq__(self, other):
-        return (self.number_of_chars_in_word == other.number_of_chars_in_word) and \
-            (self.max_number_of_continous_chars_in_word == other.max_number_of_continous_chars_in_word)
+        return (self.max_length_word_substring == other.max_length_word_substring) and \
+            (self.max_number_of_continous_chars_in_word == other.max_number_of_continous_chars_in_word) and \
+            (self.number_of_chars_in_word == other.number_of_chars_in_word)
 
     def __gt__(self, other):
-        return self.max_number_of_continous_chars_in_word > other.max_number_of_continous_chars_in_word or \
-            ((self.max_number_of_continous_chars_in_word == other.max_number_of_continous_chars_in_word) and \
+        return self.max_length_word_substring > other.max_length_word_substring or \
+            ((self.max_length_word_substring == other.max_length_word_substring) and \
+                (self.max_number_of_continous_chars_in_word > other.max_number_of_continous_chars_in_word)) or \
+            ((self.max_length_word_substring == other.max_length_word_substring) and \
+                (self.max_number_of_continous_chars_in_word == other.max_number_of_continous_chars_in_word) and \
                 (self.number_of_chars_in_word > other.number_of_chars_in_word))
