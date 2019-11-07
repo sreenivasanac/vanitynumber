b257557daf93b46070e64a71206911933846b8b5
Sreenivasan AC <sreenivasan.nitt@gmail.com>
2019-11-07 00:52:21 -0800

    Setup changes for publishing to PyPi as Pip package

--- setup.py
+++ setup.py
@@ -11,8 +11,22 @@ setup(name='vanitynumber',
       long_description=readme,
       long_description_content_type="text/markdown",
       url='https://github.com/sreenivasanac/vanitynumber',
+      keywords = ['Vanity Number', 'Toll-free number', 'Wordification', 'Marketing', 'Phone Number'],
+      install_requires=[            # I get to this in a second
+          'PyYAML',
+          'pygtrie',
+          'pytest'
+      ],
+      download_url = 'https://github.com/sreenivasanac/vanitynumber/archive/v_0.2.tar.gz',
       author='Sreenivasan AC',
       author_email='sreenivasan.nitt@gmail.com',
       license='EULA',
       packages=find_packages(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3.7',      #Specify which python versions that you want to support
+      ],
       zip_safe=False)
