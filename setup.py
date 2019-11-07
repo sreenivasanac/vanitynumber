28df8760ebd27f65e2871f32ba08a9f0fff1d24b
Sreenivasan AC <sreenivasan.nitt@gmail.com>
2019-11-07 01:31:35 -0800

    publishing v0.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(name='vanitynumber',
           'pygtrie',
           'pytest'
       ],
-      download_url = 'https://github.com/sreenivasanac/vanitynumber/archive/v_0.2.tar.gz',
+      download_url = 'https://github.com/sreenivasanac/vanitynumber/archive/v_0.3.tar.gz',
       author='Sreenivasan AC',
       author_email='sreenivasan.nitt@gmail.com',
       license='EULA',
